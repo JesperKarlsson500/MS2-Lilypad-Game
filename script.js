b8cc41cfea267b566b377bb6ed401e8b7c94990c
JesperKarlsson500 <jesper.karlsson500@gmail.com>
2021-05-31 21:15:57 +0000

    trying out the start button and creaing a for loop

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
     order = []
     playOrder = []
     
+    const disableButton = document.querySelector('on')
     let topRight = document.querySelector(".top-right");
     let bottomRight = document.querySelector(".bottom-right");
     let bottomLeft = document.querySelector(".bottom-left");
@@ -14,18 +15,33 @@
     if (style.backgroundColor === "red") {
         style.backgroundColor = "green";
         scoreCounter.innerHTML = "-";
+        disableButton = false;
     } else {
         style.backgroundColor = "red";
         scoreCounter.innerHTML = "";
+        disableButton = true;
     }
-}
+    }
+
+
 document.getElementById('on').addEventListener('click', PowerOnOffButton);
 
     // Starts the game and the computer will blink one random color
     function StartGame () {
+        if ()
+
+            
 
 
+        // clicking start will couse blue to get 50% opacity after 500ms
+        document.getElementById('start').addEventListener('click', StartGame);
     }
+
+    // test code
+    setTimeout(() => {  }, 2000);
+    //after this blue will flash for 500ms
+
+
 
     // creating a for loop to push 15 randoms number between 1 and 4 to the console
     order = [];
@@ -33,5 +49,8 @@ document.getElementById('on').addEventListener('click', PowerOnOffButton);
     for (let i = 0; i < 15; i++) {
         order.push(Math.floor(Math.random() * 4) + 1);
     }
-    console.log(order);
+
+    let currentTry = 1;
+    
+
 

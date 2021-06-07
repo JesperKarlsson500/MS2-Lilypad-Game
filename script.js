66253a380509f5867f3ec0412abc076fdb2a215d
JesperKarlsson500 <jesper.karlsson500@gmail.com>
2021-06-07 13:41:37 +0000

    opacity change when clicking colors

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
     order = []
     playOrder = []
     
-    const disableButton = document.querySelector('on')
+    let scoreCounter = document.querySelector(".score");
     let topRight = document.querySelector(".top-right");
     let bottomRight = document.querySelector(".bottom-right");
     let bottomLeft = document.querySelector(".bottom-left");
     let topLeft = document.querySelector(".top-left");
     let startButton = document.querySelector(".start");
-    let scoreCounter = document.querySelector(".score");
+    let disableButton = document.querySelector('on')
+    
 
     // Turns the game on
     function PowerOnOffButton () {
@@ -21,36 +22,41 @@
         scoreCounter.innerHTML = "";
         disableButton = true;
     }
-    }
+}
 
 
 document.getElementById('on').addEventListener('click', PowerOnOffButton);
 
     // Starts the game and the computer will blink one random color
-    function StartGame () {
-        if ()
+    // function StartGame () {
+        // if ()
 
             
 
 
         // clicking start will couse blue to get 50% opacity after 500ms
-        document.getElementById('start').addEventListener('click', StartGame);
-    }
+        //document.getElementById('start').addEventListener('click', StartGame);
+    //}
 
     // test code
-    setTimeout(() => {  }, 2000);
+    //setTimeout(() => {  }, 2000);
     //after this blue will flash for 500ms
 
 
 
     // creating a for loop to push 15 randoms number between 1 and 4 to the console
-    order = [];
+
 
     for (let i = 0; i < 15; i++) {
         order.push(Math.floor(Math.random() * 4) + 1);
     }
 
-    let currentTry = 1;
+    //let currentTry = 1;
     
 
-
+$(document).ready(function(){
+  $(".toggle").click(function(){
+    $(this).toggleClass("opacity");
+    
+  });
+});
